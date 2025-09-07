c3ce63aeab79d7b3695c02db9cbf4699ca20b121
kcw2297 <kcw2371@gmail.com>
2025-09-07 20:36:16 +0900

    chore: SAM 배포 기본 세킹

--- main.py
+++ main.py
@@ -4,13 +4,9 @@ from mangum import Mangum
 app = FastAPI()
 
 
-@app.get("/test")
-def test():
-    return {"test": "ok"}
-
-
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
 
 handler = Mangum(app)
